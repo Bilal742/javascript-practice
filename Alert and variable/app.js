0e12a2763babb573b44a54457ad0c3a4b4ea3189
Bilal <your_email@example.com>
2025-12-18 06:30:44 -0800

    JavaScript practice files update

---
+++ Alert and variable/app.js
@@ -0,0 +1,110 @@
+alert("Hello Bilal")
+
+var promt = prompt("Enter your name");
+var name = "bilal " + promt;
+console.log(name);
+name = "harry"
+console.log(name);
+
+alert("Welcome Bilal");
+alert("I ama 18 years old");
+
+var firstName = "Bilal";
+var lastName = "ali";
+lastName = "rehman"
+
+console.log(firstName + " " + lastName);
+
+var name = "Bilal";
+alert(name)
+
+var firstNumber = 100;
+var lastNumber = 200;
+var total = firstNumber + lastNumber + firstNumber * lastNumber;
+console.log(total);
+
+// var var = "variable";
+var Number = "Numbers";
+// var 11Bilal = "Worng";
+// var @$%y&7 = "Worng";
+
+var Name = "Bilal";
+var firstName = "Bilal";
+var last_name = "ali"
+
+var numbers = 10 + 5 * 7 / 8 - 10;
+console.log(numbers);
+
+var num = 10;
+total = ++num + --numbers
+console.log(total);
+
+var name = "Bilal";
+console.log(name);
+
+{
+    var firstName = "Harry";
+}
+
+function name() {
+    var firstName = "Harry";
+    // console.log(firstName);
+}
+
+console.log(name());
+
+// Globle scope
+var firstName = "Harry";
+function name() {
+    console.log(firstName);
+}
+
+{
+    let num = 10;
+    // console.log(num);  
+}
+
+console.log(num);
+
+let num = 10;
+num++;
+num--;
+console.log(num);
+
+// let num = 09;
+console.log(num);
+
+let firstName = Symbol("Harry");
+let last_name = Symbol("Harry");
+let firstName = "Harry";
+let last_name = "Harry";
+
+console.log(firstName === last_name);
+
+let name = "Bilal";
+let num = 10;
+console.log(name + num);
+
+const obj = {
+    name: "Bilal",
+    age: 18,
+    city: "karahi"
+}
+
+obj["fond"] = "harry";
+
+console.log(obj);
+
+// let num1 = 10
+// let num2 = 1;
+console.log(num1 + num2);
+console.log(++num1);
+console.log(num1++);
+
+let num1 = 10;
+console.log(++num1);
+console.log(num1);
+
+let num2 = 10;
+console.log(num2++);
+console.log(num2);   
