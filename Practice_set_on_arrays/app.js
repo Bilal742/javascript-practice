74db175737f1677f131f3977190764e173005eb0
Bilal <your_email@example.com>
2025-12-19 04:56:53 -0800

    JavaScript practice files update

--- Practice_set_on_arrays/app.js
+++ Practice_set_on_arrays/app.js
@@ -1,35 +1,42 @@
 console.log("helllo world");
 
-// let arr = [10, 20, 30, 40, 50, 60];
-// let a = prompt("Enter random number");
-// a = Number.parseInt(a);
-// arr.push(a);
-// console.log(arr);
+let arr = [10, 20, 30, 40, 50, 60];
+let a = prompt("Enter random number");
+a = Number.parseInt(a);
+arr.push(a);
+console.log(arr);
 
-// let arr = JSON.parse(localStorage.getItem("numbers")) || [10,20,30,40,50,60];
+let arr1 = JSON.parse(localStorage.getItem("numbers")) || [10, 20, 30, 40, 50, 60];
 
-// let a = prompt("Enter number");
-// a = Number(a);
+let a1 = prompt("Enter number");
+a1 = Number(a1);
 
-// arr.push(a);
+arr1.push(a1);
 
-// localStorage.setItem("numbers", JSON.stringify(arr));
+localStorage.setItem("numbers", JSON.stringify(arr1));
 
-// console.log(arr);
+console.log(arr1);
 
-// let arr = [10, 20, 30, 40, 50, 60];
-// let a;
+let arr2 = [10, 20, 30, 40, 50, 60];
+let a2;
 
-// do {
-//     a = prompt("Enter number");
-//     a = Number(a);
-//     arr.push(a);
-// } while (a != 0);
-// console.log(arr);
+do {
+    a2 = prompt("Enter number");
+    a2 = Number(a2);
+    arr2.push(a2);
+} while (a != 0);
+console.log(arr2);
 
-let arr = [1, 2, 340, 40, 540, 60];
-let n = arr.filter((c) => {
+let arr3 = [1, 2, 340, 40, 540, 60];
+let n3 = arr3.filter((c) => {
     return c % 10 == 0
+})
+
+console.log(n3);
+
+let arr4 = [1, 2, 340, 40, 1534000, 60];
+let n = arr4.map((c) => {
+    return c * c
 })
 
 console.log(n);
