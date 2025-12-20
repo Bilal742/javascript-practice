944c1cf385888c5a6f64e624ff79466150d3c8d8
Bilal <your_email@example.com>
2025-12-19 23:33:01 -0800

    🔤 Add escape sequence characters examples

--- Escape sequence character/app.js
+++ Escape sequence character/app.js
@@ -1,6 +1,5 @@
 console.log("helllo world");
 
-// Escape sequence character
 let name = "Bil\tal"
 console.log(name);
 console.log('It\rs my book');
@@ -8,4 +7,4 @@ console.log("Name:\tBilal\nAge:\t20\nCountry:\tPakistan");
 
 
 let name1 = "Bilal";
-console.log(name1.slice(0,5));
+console.log(name1.slice(0,5));  
