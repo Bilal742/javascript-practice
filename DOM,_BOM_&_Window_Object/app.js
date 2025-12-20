59610b83e3cd7488fb5e01d083c2369860827605
Bilal <your_email@example.com>
2025-12-20 03:05:33 -0800

    JavaScript practice files update

--- DOM,_BOM_&_Window_Object/app.js
+++ DOM,_BOM_&_Window_Object/app.js
@@ -1,2 +1,62 @@
 console.log("Hello World");
- 
+
+window.console.log(window)
+
+// let a = prompt("Enter your name");
+// document.querySelector("h1").innerText = `Your name is: ${a}`;
+// alert(`Your name is: ${a}`)
+
+// let a1 = prompt("Enter your father name");
+// document.querySelector("h2").innerText = `Your father name is: ${a1}`;
+// alert(`Your father name is: ${a1}`)
+
+// let color = prompt("Enter random color name");
+
+// if (color === "red") {
+//     document.body.style.background = "red"
+// }
+
+// else if (color === "blue") {
+//     document.body.style.background = "blue"
+// }
+
+// else if (color === "yellow") {
+//     document.body.style.background = "yellow"
+// }
+
+// else if (color === "black") {
+//     document.body.style.background = "black"
+// }
+
+// else if (color === "purple") {
+//     document.body.style.background = "purple"
+// }
+
+// else if (color === "orange") {
+//     document.body.style.background = "orange"
+// }
+
+// else if (color === "gray") {
+//     document.body.style.background = "gray"
+// }
+
+// else {
+//     alert("Somthing worng...")
+
+
+
+// let color = prompt("Enter any color name");
+
+// let test = document.createElement("div");
+// test.style.color = color;
+
+// if (test.style.color !== "") {
+//     document.body.style.background = color;
+// } else {
+//     alert("Invalid color name")
+// }
+
+
+let age = prompt("Enter your age");
+
+let carDrive
