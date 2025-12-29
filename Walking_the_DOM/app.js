08f413bf54b50927c8b16bc3011490b380e451f7
Bilal <your_email@example.com>
2025-12-29 13:44:15 -0800

    ✨ Update_Walking_the_DOM

--- Walking_the_DOM/app.js
+++ Walking_the_DOM/app.js
@@ -1,9 +1,13 @@
 // console.log(document.body);
 // console.log(document.documentElement);
 // document.title = "Code with harry";
-console.log(typeof(document.body));
+// console.log(typeof(document.body));
 
-let box = document.getElementById("box");
+// let box = document.getElementById("box");
 
-console.log(box); 
+// console.log(box); 
+
+document.getElementById("box").style.background = "red";
+document.getElementById("box").style.color = "blue";
+document.getElementById("box").style.paddingTop = "30px";
 
