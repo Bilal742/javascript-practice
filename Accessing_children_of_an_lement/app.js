e8b8996ec642524b14a6dd5a9ece13386c5d80db
Bilal <your_email@example.com>
2025-12-29 22:31:11 -0800

    ✨ DOM children access practice

--- Accessing_children_of_an_lement/app.js
+++ Accessing_children_of_an_lement/app.js
@@ -1,8 +1,13 @@
 console.log("Hello world");
 
-// console.log(document.body.firstChild);
-// console.log(document.children[0]);
-// console.log(document.body.firstElementChild);
-console.log(childNodes);
+console.log(document.body.firstChild);
+console.log(document.children[0]);
+console.log(document.body.firstElementChild);
+console.log(document.childNodes[0]);
+console.log(document.body.childNodes);
+
+let arr = Array.from(document.body.childNodes);
+console.log(arr[3]);
+
 
 
