110e0111aa54ab9106c15aad0ba21c5f55b434cd
Bilal <your_email@example.com>
2025-12-29 23:51:22 -0800

    🔗 Understand nextElementSibling on document.firstElementChild

--- Parents_&_Siblings_of_an_Element/app.js
+++ Parents_&_Siblings_of_an_Element/app.js
@@ -2,3 +2,13 @@ console.log("Hello World");
 
 let a = document.body.firstElementChild;
 console.log(a.parentElement);
+
+console.log(document.documentElement.parentNode);
+console.log(document.documentElement.parentElement);
+
+console.log(document.body.parentNode);
+console.log(document.body.parentElement);
+
+console.log(document.firstElementChild.nextElementSibling);
+console.log(document.firstChild.nextSibling);
+
