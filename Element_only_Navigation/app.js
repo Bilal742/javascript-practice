9a37e277e9885aa62e0d669c20d76440feec1d5c
Bilal Usman <bilalusman1291@gmail.com>
2025-12-31 15:49:01 +0500

    first commit

--- Element_only_Navigation/app.js
+++ Element_only_Navigation/app.js
@@ -6,4 +6,18 @@ let b = document.body;
 // console.log(`First Element child of b is: ${b.firstElementChild.nodeName}`);
 
 console.log(`First child of b is:`, b.firstChild);
-console.log(`First Element child of b is:`, b.firstElementChild);    
+console.log(`First Element child of b is:`, b.firstElementChild);
+
+const changeBgRed = () => {
+    document.body.firstElementChild.style.background = "black";
+    document.body.firstElementChild.style.color = "white";
+}
+
+changeBgRed();
+
+const menuIcon = document.getElementById("menuIcon")
+const navLinks = document.getElementById("navLinks");
+
+menuIcon.addEventListener("click", () => {
+    navLinks.classList.toggle("show")
+})
